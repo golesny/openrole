dee2a00bf095091bcffa8918ae0cd9a772a70ffe
Daniel Golesny <daniel@golesny.de>
2014-06-12 20:50:11 +0200

    add help for extension loading

--- webapp/app/scripts/openrole/locale.js
+++ webapp/app/scripts/openrole/locale.js
@@ -26,6 +26,19 @@ app.config(['$translateProvider', function ($translateProvider) {
     },
     BUTTON: {
       ADD: 'Add {{name}}'
+    },
+    HELP: {
+      DEVURL: {
+        TITLE: 'This help is only for Chrome browser',
+        STEP1: 'Download the folder chrome-extension from <a href="https://github.com/golesny/openrole/tree/master/examples">GitHub</a> and put it somewhere on your local disk',
+        STEP2: 'Load the extension by going to <span style="font-family: monospace">chrome://extensions</span>,'+
+          'enabling Developer Mode, and selecting the new folder with Load unpacked extension....'+
+          '<br>You will get an ID from Chrome (Example: <span style="font-family: monospace">meeofidccceehflmmmllgbafgpnnkaha</span>)',
+        STEP3: 'Enter here the Developer Extension URL (replace the ID with yours) <span style="font-family: monospace">chrome-extension://meeofidccceehflmmmllgbafgpnnkaha/pdf.dev.js</span>',
+        STEP4: 'Go to the roleplaying system you have extended and select your template',
+        STEP5: 'After modifying the js file you have to reload the page to make your changes active',
+        STEP6: 'After you have finished the template contact me so that everyone can use your template'
+      }
     }
   });
 
@@ -39,8 +52,7 @@ app.config(['$translateProvider', function ($translateProvider) {
     PDFTEMPLATE:'PDF-Template',
     MENU: {
       LOGOUT: 'Abmelden',
-      PREFERENCES: 'Einstellungen',
-      PREFERENCES_LOCAL: 'Lokale Einstellungen'
+      PREFERENCES: 'Einstellungen'
     },
     COMING: 'Kommt vielleicht mal ...',
     MSG: {
@@ -62,6 +74,19 @@ app.config(['$translateProvider', function ($translateProvider) {
     },
     PREF: {
       DEVURL: 'Entwickler Extension URL'
+    },
+    HELP: {
+      DEVURL: {
+        TITLE: 'Diese Anleitung ist für den Browser Chrome',
+        STEP1: 'Lade den Ordner chrome-extension von <a href="https://github.com/golesny/openrole/tree/master/examples">GitHub</a> und speichere ihn auf deiner lokalen Platte',
+        STEP2: 'Lade diese Extension indem <span style="font-family: monospace">chrome://extensions</span> aufrufst,'+
+        'den Entwickler Modus aktivierst und diesen Ordner in Entpackte Erweiterung laden auswählst'+
+          '<br>Dann bekommst du ein ID von Chrome (Beispiel: <span style="font-family: monospace">meeofidccceehflmmmllgbafgpnnkaha</span>)',
+        STEP3: 'Gib die Entwickler Erweiterungs URL hier ein (ersetze die ID durch deine eigene) <span style="font-family: monospace">chrome-extension://meeofidccceehflmmmllgbafgpnnkaha/pdf.dev.js</span>',
+        STEP4: 'Gehe zu dem System, dass du erweitert hast, und lade das Template',
+        STEP5: 'Nachdem du Änderungen an dem js gemacht hast, lade die Seite neu',
+        STEP6: 'Wenn du fertig bist melde dich bei mir und schicke mir das Template, dann können es alle benutzen'
+      }
     }
   })
 }]);
