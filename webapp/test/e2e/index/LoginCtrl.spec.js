66a0b86ac15948f09652efc5e25634149f01d081
Daniel Golesny <daniel@golesny.de>
2014-06-16 19:52:34 +0200

    [openrole] test for nav menu on different device sizes

--- webapp/test/e2e/index/LoginCtrl.spec.js
+++ webapp/test/e2e/index/LoginCtrl.spec.js
@@ -5,10 +5,29 @@ describe("navigation-tester", function() {
     browser.get("/webapp/app/index.html")
   });
 
-  describe("index.title", function() {
+  describe("on index.html", function() {
     it("should display correct title", function() {
       expect(browser.getTitle()).toBe("openrole.net - Character Generator Platform");
-    })
+    });
+
+    it('nav menu should be available on all device sizes', function(){
+      var menuBtn = element(by.id('smalldevmenu'));
+      var menuBtnPref = element(by.id('menuBtnPref'));
+      expect(menuBtn.isDisplayed()).toEqual(false);
+      expect(menuBtnPref.isDisplayed()).toEqual(true);
+
+      // resize an check the small device size
+      browser.driver.manage().window().setSize(500, 700);
+      browser.driver.sleep(200);
+      expect(menuBtn.isDisplayed()).toEqual(true);
+      expect(menuBtnPref.isDisplayed()).toEqual(false);
+      menuBtn.click();
+      browser.driver.sleep(100);
+      expect(menuBtnPref.isDisplayed()).toEqual(true);
+
+      // maximize to normal size
+      browser.driver.manage().window().maximize()
+    });
   });
 
 });
